a3408031543214ebe1082d061ff8e0a8d02f6b99
Matt Gardiner <matt_d_gardiner@mac.com>
2012-07-23 17:26:51 +0100

    Adding travel/coordinates endpoint

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes')
 
 routes.articles  = require('./routes/articles');
 routes.weather = require('./routes/weather');
+routes.travel = require('./routes/travel');
 
 var app = express();
 
@@ -66,6 +67,8 @@ app.get('/articles/about/:subject',routes.articles.find_by_subject);
 app.get('/weather/postcode/:postcode',routes.weather.find_by_postcode);
 
 app.get('/weather/coordinates/:lat,:lng',routes.weather.find_by_coordinates);
+
+app.get('/travel/coordinates/:lat,:lng',routes.travel.find_by_coordinates);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
---
+++ routes/travel.js
@@ -0,0 +1,121 @@
+var restler = require('restler');
+
+var async = require('async');
+
+var xml2js = require('xml2js');
+
+exports.find_by_coordinates = function(req, res){
+    
+    var lat = req.params.lat;
+    var lng = req.params.lng;
+    
+    var lookupData = {'Bedfordshire': '/travelnews/threecounties',
+            'Berkshire': '/travelnews/threecounties',
+            'Borders': '/travelnews/southscotland',
+            'Buckinghamshire': '/travelnews/threecounties',
+            'Cambridgeshire': '/travelnews/cambridgeshire',
+            'Central': '/travelnews/birmingham',
+            'Cheshire': '/travelnews/cheshire',
+            'Cornwall': '/travelnews/cornwall',
+            'County Antrim': '/travelnews/northernireland',
+            'County Armagh': '/travelnews/northernireland',
+            'County Down': '/travelnews/northernireland',
+            'County Fermanagh': '/travelnews/northernireland',
+            'County Londonderry': '/travelnews/northernireland',
+            'County Tyrone': '/travelnews/northernireland',
+            'Cumbria': '/travelnews/cumbria',
+            'Derbyshire': '/travelnews/derby',
+            'Devon': '/travelnews/devon',
+            'Dorset': '/travelnews/dorset',
+            'Dumfries and Galloway': '',
+            'Durham': '',
+            'Dyfed': '',
+            'East Sussex': '/travelnews/sussex',
+            'Essex': '/travelnews/essex',
+            'Fife': '',
+            'Gloucestershire': '/travelnews/gloucestershire',
+            'Grampian': '',
+            'Greater Manchester': '/travelnews/manchester',
+            'Gwent': '',
+            'Gwynedd County': '',
+            'Hampshire': '',
+            'Herefordshire': '',
+            'Hertfordshire': '/travelnews/threecounties',
+            'Highlands and Islands': '/travelnews/highlandsandislands',
+            'Humberside': '/travelnews/humber/2',
+            'Isle of Wight': '',
+            'Kent': '/travelnews/kent',
+            'Lancashire': '/travelnews/lancashire',
+            'Leicestershire': '/travelnews/leicester',
+            'Lincolnshire': '/travelnews/lincolnshire',
+            'Lothian': '/travelnews/southscotland',
+            'Merseyside': '',
+            'Mid Glamorgan': '/travelnews/midwales',
+            'Norfolk': '/travelnews/norfolk',
+            'North Yorkshire': '/travelnews/york',
+            'Northamptonshire': '/travelnews/northampton',
+            'Northumberland': '',
+            'Nottinghamshire': '/travelnews/nottingham',
+            'Oxfordshire': '/travelnews/oxford',
+            'Powys': '',
+            'Rutland': '',
+            'Shropshire': '/travelnews/shropshire',
+            'Somerset': '/travelnews/somerset',
+            'South Glamorgan': '/travelnews/southeastwales',
+            'South Yorkshire': '/travelnews/sheffield',
+            'Staffordshire': '',
+            'Strathclyde': '',
+            'Suffolk': '/travelnews/suffolk',
+            'Surrey': '/travelnews/surrey',
+            'Tayside': '',
+            'Tyne and Wear': '/travelnews/tyne',
+            'Warwickshire': '',
+            'West Glamorgan': '/travelnews/southwestwales',
+            'West Midlands': '/travelnews/birmingham',
+            'West Sussex': '/travelnews/sussex',
+            'West Yorkshire': '/travelnews/york',
+            'Wiltshire': '/travelnews/wiltshire',
+            'Worcestershire': '',
+    };
+
+    console.log('GEONAMES REQUEST: http://api.geonames.org/countrySubdivision?lat=' + lat + '&lng=' + lng + '&level=4&username=mdgardiner');
+
+    var geonamesRequest = restler.get('http://api.geonames.org/countrySubdivision?lat=' + lat + '&lng=' + lng + '&level=4&username=mdgardiner');
+    
+    geonamesRequest.on('complete', function(geonameResult, response){
+
+        console.log('Status Code: ' + response.statusCode);
+
+        if (geonameResult instanceof Error) {
+
+            console.log('Error: ' + geonameResult.message);
+
+        } else {
+
+            console.log('result: ' + geonameResult);
+
+            var parser = new xml2js.Parser();
+
+            var countyName = '';
+
+            parser.parseString(geonameResult, function (err, parseResult){
+                countyName = parseResult.countrySubdivision.adminName2;
+            });
+            
+            console.log('County Name: ' + countyName);
+            
+            var target = lookupData[countyName];
+
+            if ( target != '' ) {
+                console.log('REDIRECTING TO TRAVEL:  http://www.bbc.co.uk' + target + '.json');
+                res.redirect('http://www.bbc.co.uk' + target + '.json');
+            } else {
+                console.log('County (' + countyName + ') not mapped');
+                res.send(404);
+            }
+        }
+
+    });
+
+};
+
