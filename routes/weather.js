15f1285289bcea80afd9d4649a37cc3dc6d82643
Matt Gardiner <matt_d_gardiner@mac.com>
2012-07-26 13:27:13 +0100

    Tidying up and error handling

--- routes/weather.js
+++ routes/weather.js
@@ -15,7 +15,7 @@ exports.find_by_postcode = function(req, res){
     var postcode = req.params.postcode;
     
     var xmlLat = 0.0;
-    var xmllng = 0.0;
+    var xmlLng = 0.0;
     var geonameId = 0;
 
     console.log('Postcode: ' + postcode);
@@ -39,89 +39,96 @@ exports.find_by_postcode = function(req, res){
             var parser = new xml2js.Parser();
 
             parser.parseString(geonameResult, function (err, parseResult){
-                xmlLat = parseResult.code.lat;
-                xmlLng = parseResult.code.lng;
+                if (parseResult != undefined && parseResult.code != undefined) {
+                    xmlLat = parseResult.code.lat;
+                    xmlLng = parseResult.code.lng;
+                }
             });
 
             console.log('GEONAMES REQUEST DONE: la=' + xmlLat + ' lo=' + xmlLng);
 
         }
 
-        var locationUrl = 'http://open.live.bbc.co.uk/locator/locations?la=' + xmlLat + '&lo=' + xmlLng + '&format=json';
+        if (xmlLat != 0.0 && xmlLng != 0.0) {
 
-        console.log('LOCATION API REQUEST: JSON ' + locationUrl);
+            var locationUrl = 'http://open.live.bbc.co.uk/locator/locations?la=' + xmlLat + '&lo=' + xmlLng + '&format=json';
 
-        var locatorRequest = restler.get(locationUrl);
-        
-        locatorRequest.on('complete', function(locatorResult, response){
+            console.log('LOCATION API REQUEST: JSON ' + locationUrl);
 
-            console.log('Status Code: ' + response.statusCode);
+            var locatorRequest = restler.get(locationUrl);
+            
+            locatorRequest.on('complete', function(locatorResult, response){
 
-            if (locatorResult instanceof Error) {
+                console.log('Status Code: ' + response.statusCode);
 
-                console.log('Error: ' + locatorResult.message);
+                if (locatorResult instanceof Error) {
 
-            } else {
+                    console.log('Error: ' + locatorResult.message);
 
-                console.log('result: ' + JSON.stringify(locatorResult));
+                } else {
 
-                /* Have tried using JSON.parse() but it raises 
-                    the error 'Error: Failed to parse 
-                    JSON body: Unexpected token o'
+                    console.log('result: ' + JSON.stringify(locatorResult));
 
-                    Retrieving the data as XML, the xml2js parser also 
-                    errors saying that there is a non-whitespace character
-                    before the first tag and it fails to parse the data.
+                    /* Have tried using JSON.parse() but it raises 
+                        the error 'Error: Failed to parse 
+                        JSON body: Unexpected token o'
 
-                    As a last resort the eval() call works...
-                */
+                        Retrieving the data as XML, the xml2js parser also 
+                        errors saying that there is a non-whitespace character
+                        before the first tag and it fails to parse the data.
 
-                //var resultJSON = JSON.parse(locatorResult); 
-                var resultJSON = eval(locatorResult);
+                        As a last resort the eval() call works...
+                    */
 
-                geonameId = resultJSON.response.results.results[0].id;
-                
-                console.log('LOCATION REQUEST DONE: id=' + geonameId);
+                    //var resultJSON = JSON.parse(locatorResult); 
+                    var resultJSON = eval(locatorResult);
 
-                var feedname = '3dayforecast';
+                    geonameId = resultJSON.response.results.results[0].id;
+                    
+                    console.log('LOCATION REQUEST DONE: id=' + geonameId);
 
-                switch(feed){
-                    case 'day':
-                        feedname = '3dayforecast';
-                    break;
-                    case 'hour':
-                        feedname = '3hourlyforecast';
-                    break;
-                    case 'obs':
-                        feedname = 'obs';
-                    break;
-                }
-                
-                var weatherUrl = 'http://open.live.bbc.co.uk/weather/feeds/en/'+ geonameId + '/' + feedname + '.json';
+                    var feedname = '3dayforecast';
 
-                console.log('WEATHER API REQUEST (feed=' + feed + '):  ' + weatherUrl);
-                //res.redirect('http://open.live.bbc.co.uk/weather/feeds/en/'+ geonameId + '/' + feedname + '.json');
-
-                var weatherRequest = restler.get(weatherUrl);
-
-                weatherRequest.on('complete', function(weatherResult, response){
-
-                    console.log('Status Code: ' + response.StatusCode);
-
-                    if ( weatherResult instanceof Error) {
-
-                        console.log('Error: ' + weatherResult.message);
-
-                    } else {
-
-                        res.json(weatherResult);
-
+                    switch(feed){
+                        case 'day':
+                            feedname = '3dayforecast';
+                        break;
+                        case 'hour':
+                            feedname = '3hourlyforecast';
+                        break;
+                        case 'obs':
+                            feedname = 'obs';
+                        break;
                     }
+                    
+                    var weatherUrl = 'http://open.live.bbc.co.uk/weather/feeds/en/'+ geonameId + '/' + feedname + '.json';
 
-                });
-            }
+                    console.log('WEATHER API REQUEST (feed=' + feed + '):  ' + weatherUrl);
+                    //res.redirect('http://open.live.bbc.co.uk/weather/feeds/en/'+ geonameId + '/' + feedname + '.json');
 
-        });
+                    var weatherRequest = restler.get(weatherUrl);
+
+                    weatherRequest.on('complete', function(weatherResult, response){
+
+                        console.log('Status Code: ' + response.StatusCode);
+
+                        if ( weatherResult instanceof Error) {
+
+                            console.log('Error: ' + weatherResult.message);
+
+                        } else {
+
+                            res.json(weatherResult);
+
+                        }
+
+                    });
+                }
+
+            });
+        } else {
+            res.send(404);
+        }  
 
     });
 
